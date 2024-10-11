028b3955a85570526dea6a8dab72689494cbcba8
Ramesh92340 <rameshpilli1428@gmail.com>
2024-10-11 16:39:50 +0530

    admin panel logo removed

--- admin/public/sidebar.php
+++ admin/public/sidebar.php
@@ -3,7 +3,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center  bg-light text-primary" href="./index.php">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+         
         </div>
         <div class="sidebar-brand-text mx-3">P.T.School</div>
     </a>
