71f35f6ac68322f7bb7ed0d34618ed58233f07de
manimalladi <manimalladoi05@gmail.com>
2025-05-28 12:29:40 +0530

    changes

--- index.php
+++ index.php
@@ -587,6 +587,22 @@
        </section>
        <!-- End Testimonials Section -->
 
+
+   <section>
+       <div class="container">
+         <div class="row">
+           <div class="col-12">
+             <video width="100%" controls>
+               <source src="assets/img/videos/preachers.mp4" type="video/mp4">
+               Your browser does not support the video tag.
+             </video>
+           </div>
+         </div>
+       </div>
+     </section>
+
+
+
        <!-- ======= Appointment Section ======= -->
 
        <section id="appointment" class="appointment section-bg">
